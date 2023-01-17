59f01d2b886588163eba69942fb1465354cf0963
Harsh Bhandari <harshb@sahaj.ai>
2023-01-17 09:24:28 +0530

    Add remaining endpoints - Account information - Add inventory - Add money in wallet - get user order history

--- src/main/kotlin/com/esop/controller/UserController.kt
+++ src/main/kotlin/com/esop/controller/UserController.kt
@@ -18,4 +18,24 @@ class UserController {
     fun order(userName: String): String {
         return "Example Response"
     }
+
+    @Get(uri = "/{userName}/accountInformation", produces = [MediaType.APPLICATION_JSON])
+    fun getAccountInformation(userName: String): String {
+        return "Some response"
+    }
+
+    @Post(uri = "{userName}/inventory", consumes = [MediaType.APPLICATION_JSON], produces = [MediaType.APPLICATION_JSON])
+    fun addInventory(userName: String): String {
+        return "Some response"
+    }
+
+    @Post(uri = "{userName}/wallet", consumes = [MediaType.APPLICATION_JSON], produces = [MediaType.APPLICATION_JSON])
+    fun addWallet(userName: String): String {
+        return "Some response"
+    }
+
+    @Get(uri = "/{userName}/order", produces = [MediaType.APPLICATION_JSON])
+    fun getOrder(userName: String): String {
+        return "Some response"
+    }
 }

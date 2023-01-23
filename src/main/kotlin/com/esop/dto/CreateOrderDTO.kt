8be2747d772351091625ea25eed608aaf24b900c
sankar <sankaranarayananm@sahaj.ai>
2023-01-23 09:58:03 +0530

    Add validation to accept string as long value

--- src/main/kotlin/com/esop/dto/CreateOrderDTO.kt
+++ src/main/kotlin/com/esop/dto/CreateOrderDTO.kt
@@ -5,13 +5,7 @@ import com.esop.constant.MAX_WALLET_CAPACITY
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonProperty
 import io.micronaut.core.annotation.Introspected
-import javax.validation.constraints.Max
-import javax.validation.constraints.Min
-import javax.validation.constraints.NotNull
-import javax.validation.constraints.Pattern
-import javax.validation.constraints.NotBlank
-
-
+import javax.validation.constraints.*
 
 
 @Introspected
@@ -29,7 +23,7 @@ class CreateOrderDTO @JsonCreator constructor(
     @JsonProperty("price")
     @field:NotNull(message = "Price can not be missing.")
     @field:Min(1, message = "Price can not be less than zero")
-    @field:Max(MAX_WALLET_CAPACITY, message = "price can't exceed maximum wallet capacity of ${MAX_WALLET_CAPACITY.toDouble().toString()}")
+    @field:Digits(integer = 9, fraction = 0, message = "amount can't exceed maximum wallet capacity of ${MAX_WALLET_CAPACITY.toString()}")
     var price: Long? = null,
 
     @JsonProperty("inventoryType")

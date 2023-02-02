4aef41a5a6e3d9a80df5bb67a3b03fc984bd2e3f
sankar <sankaranarayananm@sahaj.ai>
2023-02-02 19:05:15 +0530

    Update UserCreationDTO to display errors with annotation

--- src/main/kotlin/com/esop/dto/UserCreationDTO.kt
+++ src/main/kotlin/com/esop/dto/UserCreationDTO.kt
@@ -1,7 +1,8 @@
 package com.esop.dto
 
-import com.esop.EmailConstraintValidator
-import com.esop.PhoneNumberConstraintValidator
+import com.esop.validators.EmailValidator
+import com.esop.validators.PhoneNumberValidator
+import com.esop.validators.UsernameValidator
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonProperty
 import io.micronaut.core.annotation.Introspected
@@ -10,11 +11,7 @@ import javax.validation.constraints.Pattern
 import javax.validation.constraints.Size
 
 
-const val PHONE_NUMBER_REGEX = "^(\\+91[\\-\\s]?)?[0]?(91)?[789]\\d{9}\$"
-
 const val USERNAME_REGEX = "^[a-zA-Z]+([a-zA-Z]|_|[0-9])*"
-
-const val EMAIL_REGEX = "(?:[a-z0-9!#\$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#\$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"
 
 const val ALPHABET_SEQUENCE_REGEX = "^\\s*[a-zA-Z]+[a-zA-Z\\s]*"
 
@@ -34,12 +31,12 @@ class UserCreationDTO @JsonCreator constructor(
 
     @JsonProperty("phoneNumber")
     @field:NotBlank(message = "Phone Number can not be missing or empty.")
-    @field:PhoneNumberConstraintValidator()
+    @field:PhoneNumberValidator()
     var phoneNumber: String? = null,
 
     @JsonProperty("email")
     @field:NotBlank(message = "Email can not be missing or empty.")
-    @field:EmailConstraintValidator()
+    @field:EmailValidator()
     var email: String? = null,
 
     @JsonProperty("username")
@@ -47,5 +44,6 @@ class UserCreationDTO @JsonCreator constructor(
     @field:Size(max=20, message = "User Name should not exceed 20 characters")
     @field:Pattern(regexp = "($USERNAME_REGEX| *)", message =
     "User Name should only consist alphabets, numbers or underscore(s) and it must start with an alphabet.")
+    @field:UsernameValidator()
     var username: String? = null
 )

447d31942f0af3f20d0113862231252a498d58f1
sankar <sankaranarayananm@sahaj.ai>
2023-02-02 16:30:13 +0530

    Add annotation for phone number

--- src/main/kotlin/com/esop/PhoneNumberConstraintValidator.kt
+++ src/main/kotlin/com/esop/PhoneNumberConstraintValidator.kt
@@ -1,4 +1,8 @@
 package com.esop
 
-class PhoneNumberconstraintValidator {
-}
+import javax.validation.Constraint
+@Retention(AnnotationRetention.RUNTIME)
+@Constraint(validatedBy = [])
+annotation class PhoneNumberConstraintValidator(
+    val message: String = "Invalid Phone Number ({validatedValue})"
+)

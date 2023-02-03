f8a324586da6b8cc58dc7e7d532855e7c7ee9b59
sankar <sankaranarayananm@sahaj.ai>
2023-02-03 14:07:29 +0530

    Update platform fee handler

--- src/main/kotlin/com/esop/schema/PlatformFee.kt
+++ src/main/kotlin/com/esop/schema/PlatformFee.kt
@@ -15,35 +15,8 @@ class PlatformFee{
             totalPlatformFee += fee.toBigInteger()
         }
 
-<<<<<<< HEAD
         fun getPlatformFee(): BigInteger {
             return totalPlatformFee
-=======
-            var feeToBeAdd: String = fee.toString()
-
-            var result:String = ""
-            platFee.reversed()
-            feeToBeAdd.reversed()
-
-            var len:Int = maxOf(platFee.length,feeToBeAdd.length)
-            var carry:Int = 0
-
-            for (i in (0..len-1)){
-                var sum: Int = carry
-                if(i< platFee.length && i<feeToBeAdd.length)
-                    sum += (platFee[i]-48).toInt()+(feeToBeAdd[i]-48).toInt()
-                else if(i< platFee.length)
-                    sum += (platFee[i]-48).toInt()
-                else
-                    sum += (feeToBeAdd[i]-48).toInt()
-                carry = sum/10
-                result += (sum%10).toString()
-            }
-            if(carry>0)
-                result += carry.toString()
-            result.reversed()
-            platFee = result
->>>>>>> 7efcd85 (Add unit testcase)
         }
 
     }

d44acafe6b8e2f9f8e7361db71df3472c5e317d5
ArunMurugan0 <122769915+ArunMurugan0@users.noreply.github.com>
2023-02-15 17:52:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-sudo su
-
 #kill process running in port 80
-lsof -t -i:80 | xargs kill -9
+sudo lsof -t -i:80 | sudo xargs kill -9
 
 #start the application
-export MICRONAUT_SERVER_PORT=80
-nohup java -jar /app/esop-0.1-all.jar > log.txt
+nohup sudo MICRONAUT_SERVER_PORT=80 java -jar /app/esop-0.1-all.jar > log.txt

6dc4c030ab2bb4deb922dd0e8374f0d5712ac0c5
Amit Bhagat <amitb@sahaj.ai>
2023-03-01 11:30:02 +0530

    update pipeline

--- main.tf
+++ main.tf
@@ -21,8 +21,39 @@ provider "aws" {
 resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
+  key_name = "gurukul-amit"
+  vpc_security_group_ids = [aws_security_group.main.id]
 
   tags = {
     Name = "terraform-instance"
   }
+}
+
+resource "aws_security_group" "main" {
+  egress = [
+    {
+      cidr_blocks      = [ "0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+ ingress                = [
+   {
+     cidr_blocks      = [ "0.0.0.0/0", ]
+     description      = ""
+     from_port        = 22
+     ipv6_cidr_blocks = []
+     prefix_list_ids  = []
+     protocol         = "tcp"
+     security_groups  = []
+     self             = false
+     to_port          = 22
+  }
+  ]
 }

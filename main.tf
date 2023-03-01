bef7305e1ec547f93079b3b3553e97095535c89f
Amit Bhagat <amitb@sahaj.ai>
2023-03-01 15:37:40 +0530

    format main.tf

--- main.tf
+++ main.tf
@@ -29,15 +29,15 @@ resource "aws_instance" "app_server" {
   }
 
   connection {
-      type     = "ssh"
-      user     = "ec2-user"
-      private_key = var.pk
-      host = self.public_ip
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = var.pk
+    host        = self.public_ip
   }
 
   provisioner "remote-exec" {
     inline = [
-	  "echo hello >> ~/msg.txt",
+      "echo hello >> ~/msg.txt",
     ]
   }
 }

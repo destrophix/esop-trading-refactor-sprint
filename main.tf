834530c87722c2a051f9823d09010c888f854149
Amit Bhagat <amitb@sahaj.ai>
2023-03-01 14:21:59 +0530

    allow incoming requests at port 80

--- main.tf
+++ main.tf
@@ -54,6 +54,17 @@ resource "aws_security_group" "main" {
       security_groups  = []
       self             = false
       to_port          = 22
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 80
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 80
     }
   ]
 }
